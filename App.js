882d3e233e865c525c4cfe0aae9abc52b17d0e6e
ZAchrrab <zachrrab@gmail.com>
2022-04-07 14:57:45 +0000

    Display my current location

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React  from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator} from 'react-navigation';
@@ -25,4 +26,3 @@ export default () => {
   />
   );
 }
-
